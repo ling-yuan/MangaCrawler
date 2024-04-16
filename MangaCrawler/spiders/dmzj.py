2d7943860de2e809c8a9ea4404214975126763c9
baishi <572268633@qq.com>
2024-04-16 21:38:38 +0800

    modified:   dmzj.py

--- MangaCrawler/spiders/dmzj.py
+++ MangaCrawler/spiders/dmzj.py
@@ -1,7 +1,13 @@
+import time
 import scrapy
 from scrapy.http import JsonRequest, Response
 
-from MangaCrawler.items import MhItem
+from MangaCrawler.items import ChapterItem, MhItem
+
+
+# 时间戳转换
+def ts_to_time(ts):
+    return time.strftime('%Y/%m/%d', time.localtime(int(ts)))
 
 
 class DmzjSpider(scrapy.Spider):
@@ -48,7 +54,7 @@ class DmzjSpider(scrapy.Spider):
             url = url[:-1]
 
             # 负载不同，但url相同，需要设置dont_filter=True，否则会被调度器认为是重复请求
-            yield scrapy.Request(url=url, callback=self.parse_chapter_list, cb_kwargs={'mh_item': mh_item}, dont_filter=True)
+            yield scrapy.Request(url=url, callback=self.parse_chapter_list, cb_kwargs={'mh_item': mh_item, 'comic_py': comic["comic_py"]}, dont_filter=True)
 
     def parse_chapter_list(self, response: Response, **kwargs):
         '''解析漫画章节列表'''
@@ -59,10 +65,49 @@ class DmzjSpider(scrapy.Spider):
         if errno != 0:
             yield mh_item
         else:
+            mh_id = data['data']['comicInfo']['id']
             chapter_list = data['data']['comicInfo']['chapterList']
             if chapter_list == None:
                 yield mh_item
             else:
                 chapter_list = chapter_list[0]['data']
+                mh_item['mh_chapter_length'] = len(chapter_list)
                 print(chapter_list)
-                yield mh_item
+                i = 0
+                for chapter in chapter_list:
+                    url = 'https://www.idmzj.com/api/v1/comic1/chapter/detail?'
+                    params = {
+                        'channel': 'pc',
+                        'app_name': 'dmzj',
+                        'version': '1.0.0',
+                        'comic_id': mh_id,
+                        'chapter_id': chapter['chapter_id'],
+                    }
+
+                    for key, value in params.items():
+                        url = url + f'{key}={value}&'
+                    url = url[:-1]
+
+                    chapter_url = response.urljoin(f"/view/{kwargs['comic_py']}/{chapter['chapter_id']}.html")
+                    chapter_time_ts = chapter['updatetime']
+
+                    yield scrapy.Request(url=url, callback=self.parse_chapter_content, cb_kwargs={'mh_item': mh_item, 'chapter_url': chapter_url, 'chapter_num': i, 'chapter_time': chapter_time_ts}, dont_filter=True)
+
+    def parse_chapter_content(self, response: Response, **kwargs):
+        '''解析漫画章节内容'''
+        mh_item = kwargs['mh_item']
+        data = response.json()
+
+        chapter_item = ChapterItem()
+        chapter_item['chapter_num'] = kwargs['chapter_num']
+        chapter_item['chapter_title'] = data['data']['chapterInfo']['title']
+        chapter_item['chapter_time'] = ts_to_time(kwargs['chapter_time'])
+        chapter_item['chapter_url'] = kwargs['chapter_url']
+        chapter_item['chapter_content_url_list'] = data['data']['chapterInfo']['page_url']
+
+        mh_item['mh_chapter_list'].append(chapter_item)
+
+        if mh_item['mh_chapter_length'] == len(mh_item['mh_chapter_list']):
+            yield mh_item
+        # else:
+        #     yield None
